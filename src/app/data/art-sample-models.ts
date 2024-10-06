5c8b3c9c750b57cde423dbc812b815b96119b5c0
tomioka-tsukasa <tsukasa.creator@gmail.com>
2024-10-06 21:17:25 +0900

    add: 全てのアート作品をダウンロード&表示

--- src/app/data/art-sample-models.ts
+++ src/app/data/art-sample-models.ts
@@ -1,114 +1,245 @@
-import sampleVisual from "¥/public/assets/art-001.jpg"
+import artImage001 from "¥/public/assets/art-001.jpg"
+import artImage002 from "¥/public/assets/art-002.jpg"
+import artImage003 from "¥/public/assets/art-003.jpg"
+import artImage004 from "¥/public/assets/art-004.jpg"
+import artImage005 from "¥/public/assets/art-005.jpg"
+import artImage006 from "¥/public/assets/art-006.jpg"
+import artImage007 from "¥/public/assets/art-007.jpg"
+import artImage008 from "¥/public/assets/art-008.jpg"
+import artImage009 from "¥/public/assets/art-009.jpg"
+import artImage010 from "¥/public/assets/art-010.jpg"
+import artImage011 from "¥/public/assets/art-011.jpg"
+import artImage012 from "¥/public/assets/art-012.jpg"
+import artImage013 from "¥/public/assets/art-013.jpg"
+import artImage014 from "¥/public/assets/art-014.jpg"
+import artImage015 from "¥/public/assets/art-015.jpg"
+import artImage016 from "¥/public/assets/art-016.jpg"
+import artImage017 from "¥/public/assets/art-017.jpg"
+import artImage018 from "¥/public/assets/art-018.jpg"
+import artImage019 from "¥/public/assets/art-019.jpg"
+import artImage020 from "¥/public/assets/art-020.jpg"
+import artImage021 from "¥/public/assets/art-021.jpg"
+import artImage022 from "¥/public/assets/art-022.jpg"
 
 export const ArtSampleModels: Array<Cms.ArtModel> = [
   {
     slug: 'art-001',
-    title: 'Fabrica. Exp - 001',
+    title: 'Art - 001',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage001,
       altText: '',
     }
   },
   {
     slug: 'art-002',
-    title: 'Fabrica. Exp - 002',
+    title: 'Art - 002',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage002,
       altText: '',
     }
   },
   {
     slug: 'art-003',
-    title: 'Fabrica. Exp - 003',
+    title: 'Art - 003',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage003,
       altText: '',
     }
   },
   {
     slug: 'art-004',
-    title: 'Fabrica. Exp - 004',
+    title: 'Art - 004',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage004,
       altText: '',
     }
   },
   {
     slug: 'art-005',
-    title: 'Fabrica. Exp - 005',
+    title: 'Art - 005',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage005,
       altText: '',
     }
   },
   {
     slug: 'art-006',
-    title: 'Fabrica. Exp - 006',
+    title: 'Art - 006',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage006,
       altText: '',
     }
   },
   {
     slug: 'art-007',
-    title: 'Fabrica. Exp - 007',
+    title: 'Art - 007',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage007,
       altText: '',
     }
   },
   {
     slug: 'art-008',
-    title: 'Fabrica. Exp - 008',
+    title: 'Art - 008',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage008,
       altText: '',
     }
   },
   {
     slug: 'art-009',
-    title: 'Fabrica. Exp - 009',
+    title: 'Art - 009',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage009,
       altText: '',
     }
   },
   {
     slug: 'art-010',
-    title: 'Fabrica. Exp - 010',
+    title: 'Art - 010',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage010,
       altText: '',
     }
   },
   {
     slug: 'art-011',
-    title: 'Fabrica. Exp - 011',
+    title: 'Art - 011',
     enTitle: '',
-    author: 'Tsukasa Tomioka.',
+    author: 'Art Author.',
     visual: {
-      ...sampleVisual,
+      ...artImage011,
       altText: '',
     }
   },
-];
+  {
+    slug: 'art-012',
+    title: 'Art - 012',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage012,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-013',
+    title: 'Art - 013',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage013,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-014',
+    title: 'Art - 014',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage014,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-015',
+    title: 'Art - 015',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage015,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-016',
+    title: 'Art - 016',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage016,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-017',
+    title: 'Art - 017',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage017,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-018',
+    title: 'Art - 018',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage018,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-019',
+    title: 'Art - 019',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage019,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-020',
+    title: 'Art - 020',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage020,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-021',
+    title: 'Art - 021',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage021,
+      altText: '',
+    }
+  },
+  {
+    slug: 'art-022',
+    title: 'Art - 022',
+    enTitle: '',
+    author: 'Art Author.',
+    visual: {
+      ...artImage022,
+      altText: '',
+    }
+  },
+]
